439470a2851b0bd240c18868b861665cb5f2c77b
baozy2000 <baozy2000@163.com>
2025-06-19 09:22:42 +0800

    添加项目文件。

---
+++ init.cpp
@@ -0,0 +1,101 @@
+#include <zinx.h>
+#include <iostream>
+
+using namespace std;
+
+/*2-功能处理类*/
+class Echo :public AZinxHandler {
+	virtual IZinxMsg* InternelHandle(IZinxMsg& _oInput) override {
+		/*回显到标准输出*/
+		GET_REF2DATA(BytesMsg, input, _oInput);
+		//cout << input.szData << endl;
+        ZinxKernel::Zinx_Send0ut(input.szData, *poOut);
+		return nullptr;
+	}
+	virtual AZinxHandler* GetNextHandler(IZinxMsg& _oNextMsg) override
+	{
+		return nullptr;
+	}
+}*poEcho = new Echo();
+
+/*3-通道类*/
+class TestStdin :public Ichannel {
+    // 通过 Ichannel 继承
+    virtual bool Init() override
+    {
+        return true;
+    }
+    virtual bool ReadFd(std::string& _input) override
+    {
+        cin >> _input;
+        return true;
+    }
+    virtual bool WriteFd(std::string& _output) override
+    {
+        return false;
+    }
+    virtual void Fini() override
+    {
+    }
+    virtual int GetFd() override
+    {
+        return 0;
+    }
+    virtual std::string GetChannelInfo() override
+    {
+        return "stdin";
+    }
+    virtual AZinxHandler* GetInputNextStage(BytesMsg& _oInput) override
+    {
+        return poEcho;
+    }
+};
+
+class TestStdout :public Ichannel {
+    // 通过 Ichannel 继承
+    virtual bool Init() override
+    {
+        return true;
+    }
+    virtual bool ReadFd(std::string& _input) override
+    {
+        return false;
+    }
+    virtual bool WriteFd(std::string& _output) override
+    {
+        cout << _output << endl;
+        return true;
+    }
+    virtual void Fini() override
+    {
+    }
+    virtual int GetFd() override
+    {
+        return 1;
+    }
+    virtual std::string GetChannelInfo() override
+    {
+        return "stdout";
+    }
+    virtual AZinxHandler* GetInputNextStage(BytesMsg& _oInput) override
+    {
+        return nullptr;
+    }
+} *poOut = new TestStdout();
+
+int main()
+{
+    /*1-初始化框架*/
+    ZinxKernel::ZinxKernelInit();
+
+    /*4-将通道对象添加到框架*/
+    TestStdin* poStdin = new TestStdin();
+    ZinxKernel::Zinx_Add_Channel(*poStdin); 
+    ZinxKernel::Zinx_Add_Channel(*poOut);
+
+    /*5-运行框架*/
+    ZinxKernel::Zinx_Run();
+
+    ZinxKernel::ZinxKernelFini();
+    return 0;
+}
